68660c2042a9ee92a37c917748f152faabba7b01
Andrei Bobkov <slider7259@gmail.com>
2025-06-10 13:49:15 +0200

    feat: favicons

--- app/root.tsx
+++ app/root.tsx
@@ -22,6 +22,18 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "1024x1024",
+    href: "/icon.png",
+  },
+  {
+    rel: "apple-touch-icon",
+    href: "/apple-icon.png",
+    type: "image/png",
+    sizes: "1024x1024",
+  },
 ]
 
 export function Layout({ children }: { children: ReactNode }) {
